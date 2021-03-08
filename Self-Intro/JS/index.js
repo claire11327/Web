1cc4e7457f55c53ce9cb0f72b1dbd90c8fc9916f
claire11327 <claire11327@gmail.com>
2021-03-08 12:52:07 +0800

    Add img bar

--- Self-Intro/JS/index.js
+++ Self-Intro/JS/index.js
@@ -8,9 +8,17 @@ function changeImg(){
     var path = "SRC/IMG/1. Home/";
     var imgs = new Array('IMG01.jpg', 'IMG02.jpg', 'IMG03.jpg');
     var element = document.getElementById("HomeImg");
+    var elementOrders = document.getElementById("HomeImgContainer").getElementsByTagName('svg');
     
     img.src = path+imgs[i];
     element.src = img.src;
+    if(elementOrders) {
+        for(var j = 0; j < 3; j++){
+            elementOrders[j].style.backgroundColor = '#E7DDDE';    
+        }
+        setTimeout(function(){},500);
+        elementOrders[i].style.backgroundColor = '#AAAEB5';
+    }
     
     i ++;
     i %= 3;
