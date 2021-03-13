4b1d669a7dcfaaaa048b5b5567bced51f158ea6c
claire11327 <claire11327@gmail.com>
2021-03-14 01:57:39 +0800

    Fixed JS: divid changeOrder() to changeImg()

--- Self-Intro/JS/index.js
+++ Self-Intro/JS/index.js
@@ -1,5 +1,6 @@
 var i = 1;
 function onloadImg(){
+    setInterval('changePage()',3000);
     setInterval('changeImg()',3000);
 }
 
@@ -8,21 +9,21 @@ function changeImg(){
     var path = "SRC/IMG/1. Home/";
     var imgs = new Array('IMG01.jpg', 'IMG02.jpg', 'IMG03.jpg');
     var element = document.getElementById("HomeImg");
-    var elementOrders = document.getElementById("HomeImgContainer").getElementsByTagName('svg');
     
     img.src = path+imgs[i];
     element.src = img.src;
-    if(elementOrders) {
-        for(var j = 0; j < 3; j++){
-            elementOrders[j].style.backgroundColor = '#E7DDDE';    
-        }
-        setTimeout(function(){},500);
-        elementOrders[i].style.backgroundColor = '#AAAEB5';
-    }
     
     i ++;
     i %= 3;
 }
 
+function changePage(){
+    var elementOrders = document.getElementById("HomeImgContainer").getElementsByTagName('svg');
+    var j = i > 0 ? i-1: 2;
+    elementOrders[i].style.backgroundColor = '#AAAEB5';
+    elementOrders[j].style.backgroundColor = '#E7DDDE';    
+
+
+}
 
   
