129331caebe68340080978cd3f320c873620b851
claire11327 <claire11327@gmail.com>
2021-03-18 01:29:26 +0800

    Add Folder: docs

--- Self-Intro/JS/index.js
+++ Self-Intro/JS/index.js
@@ -6,7 +6,7 @@ function onloadImg(){
 
 function changeImg(){
     var img = new Image();
-    var path = "SRC/IMG/1. Home/";
+    var path = "../SRC/IMG/1. Home/";
     var imgs = new Array('IMG01.jpg', 'IMG02.jpg', 'IMG03.jpg');
     var element = document.getElementById("HomeImg");
     
