3213486094c323dcc9297e0ad757b5cd64e5354a
claire11327 <claire11327@gmail.com>
2021-06-29 23:13:05 +0800

    RWD

--- About-us_Pre/js/index.js
+++ About-us_Pre/js/index.js
@@ -1,23 +1,31 @@
+$(window).on('load',IntroRWD())
+
 $(window).on('scroll', function() {
-    if(isScrolledIntoView($('#Home'))){
+    if(isScrolledIntoViewVtl($('#Home'))){
         $('.nav-items a[href="#Home"]').addClass('scrolling');
     }else{
         $('.nav-items a[href="#Home"]').removeClass('scrolling');
     }
 
-    if(isScrolledIntoView($('#Intro'))){
+    if(isScrolledIntoViewVtl($('#Intro'))){
         $('.nav-items a[href="#Intro"]').addClass('scrolling');
     }else{
         $('.nav-items a[href="#Intro"]').removeClass('scrolling');
     }
     
-    if(isScrolledIntoView($('#Use'))){
+    if(isScrolledIntoViewVtl($('#Use'))){
         $('.nav-items a[href="#Use"]').addClass('scrolling');
     }else{
         $('.nav-items a[href="#Use"]').removeClass('scrolling');
     }
 
-    if(isScrolledIntoView($('#About'))){
+    if(isScrolledIntoViewVtl($('#About'))){
+        $('.nav-items a[href="#About"]').addClass('scrolling');
+    }else{
+        $('.nav-items a[href="#About"]').removeClass('scrolling');
+    }
+
+    if(isScrolledIntoViewHzl($('#About'))){
         $('.nav-items a[href="#About"]').addClass('scrolling');
     }else{
         $('.nav-items a[href="#About"]').removeClass('scrolling');
@@ -25,7 +33,7 @@ $(window).on('scroll', function() {
 
 })
 
-function isScrolledIntoView(elem) {
+function isScrolledIntoViewVtl(elem) {
     var docViewTop = $(window).scrollTop()-$('nav').height();
     var docViewBottom = docViewTop + $(window).height();
   
@@ -33,6 +41,32 @@ function isScrolledIntoView(elem) {
     var elemBottom = elemTop + ($(elem).height()*2/3);
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
+
+function isScrolledIntoViewHzl(elem) {
+    var docViewTop = $(window).scrollTop()-$('nav').height();
+    var docViewBottom = docViewTop + $(window).height();
+  
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + ($(elem).height()*2/3);
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+
+
+function isScrolledIntoViewVtl(elem) {
+    var docViewTop = $(window).scrollTop()-$('nav').height();
+    var docViewBottom = docViewTop + $(window).height();
+  
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + ($(elem).height()*2/3);
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+
+
+
+
+
 
 $("nav a[href='#Use']").on("click", function(e){
     var offset = $('#Home').outerHeight() + $('#Intro').outerHeight();
